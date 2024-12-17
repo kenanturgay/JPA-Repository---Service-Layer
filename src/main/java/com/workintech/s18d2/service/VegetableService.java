d86a783e5c3d364eceda0cc16eb4ba8dc9747fff
Kenan Turgay <knnturgay@gmail.com>
2024-12-17 14:54:23 +0300

    Create VegetableService.java
    VegetableService was created

---
+++ src/main/java/com/workintech/s18d2/service/VegetableService.java
@@ -0,0 +1,21 @@
+package com.workintech.s18d2.service;
+
+import com.workintech.s18d2.entity.Vegetable;
+import com.workintech.s18d2.entity.Vegetable;
+
+import java.util.List;
+
+public interface VegetableService {
+
+    List<Vegetable> getByPriceAsc();
+
+    List<Vegetable> getByPriceDesc();
+
+    Vegetable getById(Long id);
+
+    Vegetable save(Vegetable vegetable);
+
+    Vegetable delete(Long id);
+
+    List<Vegetable> searchByName(String name);
+}
