5af7108789a2715e4f448ff5bda9aa723e360758
Kenan Turgay <knnturgay@gmail.com>
2024-12-17 12:07:57 +0300

    Create Plant.java
    Plant class was created

---
+++ src/main/java/com/workintech/s18d2/entity/Plant.java
@@ -0,0 +1,34 @@
+package com.workintech.s18d2.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+
+
+public class Plant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull(message = "name cannot be null")
+    @Size(min = 2 , max = 45, message = "name must be between 2 and 45")
+    private String name;
+
+    @Column(name = "price")
+    @DecimalMin("10")
+    private Double price;
+
+
+}
